991e77a8e3f00c31e3ad7c93e2be3fae7a4e0ad5
deaving-71 <110570275+deaving-71@users.noreply.github.com>
2024-05-11 20:53:24 +0100

    add: error handling to the login handler

--- backend/app/controllers/auth/login_controller.ts
+++ backend/app/controllers/auth/login_controller.ts
@@ -5,27 +5,27 @@ import * as querystring from 'node:querystring'
 
 export default class LoginController {
   async store({ request, response, auth }: HttpContext) {
-    // try {
-    const { email, password, remember } = await request.validateUsing(loginValidator)
+    try {
+      const { email, password, remember } = await request.validateUsing(loginValidator)
 
-    const user = await User.verifyCredentials(email, password)
+      const user = await User.verifyCredentials(email, password)
 
-    await auth.use('web').login(user, remember)
+      await auth.use('web').login(user, remember)
 
-    response.plainCookie('noirechat-userdata', querystring.stringify(user.serialize()), {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      encode: false,
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-    })
+      response.plainCookie('noirechat-userdata', querystring.stringify(user.serialize()), {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        encode: false,
+        maxAge: 1000 * 60 * 60 * 24 * 30,
+      })
 
-    return response.json(user)
-    // } catch (error) {
-    //   return response.badRequest({
-    //     errors: [{ message: 'Invalid email or password', field: 'root', rule: 'invalid_form' }],
-    //   })
-    // }
+      return response.json(user)
+    } catch (error) {
+      return response.badRequest({
+        errors: [{ message: 'Invalid email or password', field: 'root', rule: 'invalid_form' }],
+      })
+    }
   }
 
   async destroy({ auth }: HttpContext) {
